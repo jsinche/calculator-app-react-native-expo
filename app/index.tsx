8bda3cd44e4ebfa6e1d232f091b4b6eb1cf5a277
JeanS <jean.sinche@alarcongroup.us>
2024-12-19 17:42:48 -0500

    Improves main result text display
    Truncates long numbers in the main result display to prevent overflow.

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,13 @@ import { globalStyles } from "@/styles/global-styles";
 const CalculatorApp = () => {
   return (
     <View style={globalStyles.calculatorContainer}>
-      <Text style={globalStyles.mainResult}>50 x 50</Text>
+      <Text
+        style={globalStyles.mainResult}
+        numberOfLines={1}
+        adjustsFontSizeToFit
+      >
+        50 x 5000000000
+      </Text>
       <Text style={globalStyles.subResult}>2500</Text>
     </View>
   );
