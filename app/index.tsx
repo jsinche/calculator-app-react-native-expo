58e4aaa48d834f921a53ce1c9ecd61b3f277c321
JeanS <jean.sinche@alarcongroup.us>
2024-12-19 17:36:19 -0500

    Sets up basic calculator UI
    Initializes the calculator app with a basic UI displaying the main and sub results.
    
    Adds basic styling using global styles and color constants.  Removes previous navigation and theme provider setup.

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { globalStyles } from "@/styles/global-styles";
+
+const CalculatorApp = () => {
+  return (
+    <View style={globalStyles.calculatorContainer}>
+      <Text style={globalStyles.mainResult}>50 x 50</Text>
+      <Text style={globalStyles.subResult}>2500</Text>
+    </View>
+  );
+};
+
+export default CalculatorApp;
